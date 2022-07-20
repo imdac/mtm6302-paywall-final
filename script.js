5b008a70823c8d6cfc6df8e0d1d9ef2c180726c9
Michael Eisenbraun <eisenbm@algonquincollege.com>
2022-07-20 18:19:29 -0400

    Creates the final version

---
+++ script.js
@@ -0,0 +1,8 @@
+// retrieve the modal
+const $modal = document.getElementById('modal')
+
+// delay adding class for 3 seconds
+setTimeout(function () {
+  // add the show class to modal
+  $modal.classList.add('show')
+}, 3000)
